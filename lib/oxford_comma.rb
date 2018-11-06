90cbedb2347551cf3ab67723ee9725015d8b880f
NaKia Whitby <whitby.nakia@gmail.com>
2018-11-06 16:16:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,3 @@
 def oxford_comma(array)
-  if array.length == 1
-  "#{array[0]}"
-elsif array.length == 2
-  array.join (" and ")
-elsif array.length >= 3
-  array[-1] = "and #{array[-1]}"
-  array.
+
 end
